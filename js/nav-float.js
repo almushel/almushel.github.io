9e61a1d3af279fe0053fbd8641dd3eea4b634a6a
Andrew Mushel <almushel@gmail.com>
2020-01-08 18:34:29 -0600

    Simple mobile-first CSS rules.

--- js/nav-float.js
+++ js/nav-float.js
@@ -13,6 +13,7 @@ function floatNav() {
     if (rect.top < 0) {
         origin = document.documentElement.scrollTop + rect.top;
         float.style.display = 'initial';
+        float.style.zIndex = '2';
         static.style.visibility = "hidden"
     } else if (document.documentElement.scrollTop < origin) {
         float.style.display = 'none';
