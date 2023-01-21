86a4e8c2fb2d5511620e46fa924d94cbd93ac4cc
Andrew Mushel <almushel@gmail.com>
2023-01-21 02:24:08 -0600

    Style tag-filter controls

--- js/tag-filter.js
+++ js/tag-filter.js
@@ -1,16 +1,74 @@
+let styles = document.createElement("style");
+	styles.innerHTML = `
+		.filter-container {
+			overflow: hidden;
+			
+			width: 49%;
+			margin: 0% 1% 1% 0;
+			
+			background: var(--bg-color-1);
+			border: 1px solid rgba(0,0,0,0);
+
+			transition: height .25s;
+		}
+
+		.filter-container:hover {
+			border: 1px solid var(--accent-color);
+		}
+
+		.filter-container label {
+			vertical-align: top;
+		}
+
+		.filter-container > span {
+			display: block;
+		}
+
+		.filter-container .label {
+			padding: 1.25%;
+			margin-bottom: .25em;
+			border-bottom: 1px solid var(--bg-color-3);
+			cursor: pointer;
+		}
+
+		.filter-container .label:hover {
+			background: var(--bg-color-2);
+		}
+
+		@media only screen and (min-width: 1280px) {
+			.filter-container {
+				width: 32.3%;
+			}
+		}
+	`
+
 export default class TagFilter extends HTMLElement {
 	constructor() {
 		super();
 		this.elements = Array.from(this.children);
 		
+		const labelHeight = "1.6em";
+
 		this.tagNames = new Set();
 		this.tags = {};
 		this.tagInputs = document.createElement("div");
+		this.tagInputs.className = "filter-container";
+		this.tagInputs.style.height = labelHeight;
+
+		let containerLabel = document.createElement("span");
+		containerLabel.innerHTML = "Filters";
+		containerLabel.classList.add("label");
+		containerLabel.onclick = (e) => {
+			e.target.parentElement.style.height = (e.target.parentElement.style.height === labelHeight) ? (0.25 + this.tagNames.size * 1.5)+"em" : labelHeight;
+		}
+
+		this.tagInputs.append(containerLabel);
 		
 //		this.categoryNames = new Set();
 //		this.categories = {};
 		
 		const shadowRoot = this.attachShadow({mode: "open"});
+		shadowRoot.append(styles.cloneNode(true));
 
 		for (let child of this.elements) {
 			for (let tag of this.extractElementTags(child)) this.pushTag(tag);
@@ -37,7 +95,7 @@ export default class TagFilter extends HTMLElement {
 
 		for (let i of tagInputsArr) this.tagInputs.append(i);
 
-		shadowRoot.prepend(this.tagInputs);
+		if (this.tagNames.size) shadowRoot.prepend(this.tagInputs);
 /*	
 		for (let name of this.categoryNames) {
 			let heading = document.createElement("h3");
@@ -68,7 +126,7 @@ export default class TagFilter extends HTMLElement {
 			this.tags[tagName] = false;
 
 			let container = document.createElement("span");
-				container.title = tagName
+				container.title = tagName;
 					
 			let checkbox = document.createElement("input");
 				checkbox.type = "checkbox";
