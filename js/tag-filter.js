e61ddc87c14969c5a7ff4d5aae3aa2e421e048c1
Andrew Mushel <almushel@gmail.com>
2023-01-24 23:00:07 -0600

    Improve filter styles and menu height calculation

--- js/tag-filter.js
+++ js/tag-filter.js
@@ -1,84 +1,85 @@
-let styles = document.createElement("style");
-	styles.innerHTML = `
-		.filter-container {
-			overflow: hidden;
-			
-			width: 49%;
-			height: auto;
-			margin: 0% 1% 1% 0;
-			
-			background: var(--bg-color-1);
-			border: 1px solid rgba(0,0,0,0);
+const styleSheet = new CSSStyleSheet()
+styleSheet.replaceSync(`
+.filter-container {
+	overflow: hidden;
+	
+	width: 49%;
+	height: var(--container-collapsed-height, auto);
+	margin: 0% 1% 1% 0;
+	
+	background: var(--bg-color-1);
+	border: 1px solid rgba(0,0,0,0);
 
-			transition: height .25s;
-		}
+	transition: height .25s;
+}
 
-		.filter-container:hover {
-			border: 1px solid var(--accent-color);
-		}
+.filter-container:hover {
+	border: 1px solid var(--accent-color);
+}
 
-		.filter-container label {
-			vertical-align: top;
-		}
+.filter-container label {
+	vertical-align: top;
+}
 
-		.filter-container > span {
-			display: block;
-			padding-bottom: 0.25em;
-			margin: 0;
-		}
+.filter-container > span {
+	display: block;
+	padding-bottom: 0.25em;
+	margin: 0;
+}
 
-		.filter-container .toggle {
-			display: block;
-			padding: .25em;
-			margin-bottom: .25em;
-			border-bottom: 1px solid var(--bg-color-3);
-			cursor: pointer;
-		}
+.filter-container .toggle {
+	display: block;
+	padding: .25em;
+	margin-bottom: .25em;
+	border-bottom: 1px solid var(--bg-color-3);
+	cursor: pointer;
+}
 
-		.filter-container .toggle:hover {
-			background: var(--bg-color-2);
-		}
+.filter-container .toggle:hover {
+	background: var(--bg-color-2);
+}
 
-		.arrow {
-			border: solid white;
-			border-width: 0 3px 3px 0;
-			display: inline-block;
-			margin-right: .25em;
-			
-			padding: .25em;
+.filter-container.expanded {
+	height: var(--container-expanded-height, auto);
+}
 
-			transition: transform .25s;
-		}
+.arrow {
+	border: solid white;
+	border-width: 0 3px 3px 0;
+	display: inline-block;
+	margin-right: .25em;
+	
+	padding: .25em;
 
-		.up {
-			transform: translateY(50%) rotate(-135deg);
-			-webkit-transform: translateY(50%) rotate(-135deg);
-		}
-		  
-		.down {
-			transform: translateY(50%) rotate(45deg);
-			-webkit-transform: translateY(50%) rotate(45deg);
-		}
+	transition: transform .25s;
+	transform: translateY(50%) rotate(-135deg);
+	-webkit-transform: translateY(50%) rotate(-135deg);
+}
+  
+.expanded .arrow {
+	transform: translateY(50%) rotate(45deg);
+	-webkit-transform: translateY(50%) rotate(45deg);
+}
 
-		@media only screen and (min-width: 1280px) {
-			.filter-container {
-				width: 32.3%;
-			}
-		}
-	`
+@media only screen and (min-width: 1280px) {
+	.filter-container {
+		width: 32.3%;
+	}
+}
+`);
 
 export default class TagFilter extends HTMLElement {
 	constructor() {
 		super();
-		this.elements = Array.from(this.children);
+		const shadowRoot = this.attachShadow({mode: "closed"});
+		shadowRoot.adoptedStyleSheets = [styleSheet];
 
+		this.elements = Array.from(this.children);
 		this.tagNames = new Set();
 		this.tags = {};
 		this.tagControls = document.createElement("div");
 		this.tagControls.className = "filter-container";
-		
-		const shadowRoot = this.attachShadow({mode: "closed"});
-		shadowRoot.append(styles.cloneNode(true));
+		shadowRoot.append(this.tagControls);
 
 		for (let child of this.elements) {
 			for (let tag of this.extractElementTags(child)) this.pushTag(tag);
@@ -104,8 +105,6 @@ export default class TagFilter extends HTMLElement {
 				containerToggle.classList.add("toggle");
 	
 			this.tagControls.prepend(containerToggle);
-			
-			shadowRoot.prepend(this.tagControls);
 		}
 
 		this.updateTagFilter()
@@ -118,14 +117,10 @@ export default class TagFilter extends HTMLElement {
 		// NOTE: expandedHeight is always short the equivalent of one checkbox span padding-bottom value (.25em or 4px)
 		const expandedHeight = (toggleHeight+1) + this.tagNames.size * checkBoxHeight;
 
-		this.tagControls.style.height = toggleHeight.toFixed(1)+"px";
-		toggle.onclick = () => {
-			let newHeight = (toggle.parentElement.style.height == toggleHeight.toFixed(1)+"px") ? expandedHeight : toggleHeight;
-			toggle.parentElement.style.height = newHeight.toFixed(1)+"px";
-			
-			toggle.querySelector(".arrow").classList.toggle("down")
-			toggle.querySelector(".arrow").classList.toggle("up");
-		}
+		styleSheet.insertRule(`:host { --container-collapsed-height: ${toggleHeight+"px"} }`);
+		styleSheet.insertRule(`:host { --container-expanded-height: ${expandedHeight+"px"} }`);
+
+		toggle.onclick = () => { toggle.parentElement.classList.toggle("expanded"); }
 	}
 
 	pushTag(tagName) {
