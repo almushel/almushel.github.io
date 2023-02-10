95a1677e09c3ae1c7b319bdaec22db40260a1c1d
Andrew Mushel <almushel@gmail.com>
2023-02-10 11:16:57 -0600

    audio-playlist loop button

--- js/audio-playlist.js
+++ js/audio-playlist.js
@@ -1,52 +1,5 @@
 const stylesheet = new CSSStyleSheet();
 stylesheet.replaceSync(`
-	audio {
-		margin: .5em;
-		width: calc(100% - 1em);
-	}
-
-	.playlist-container {
-		background: var(--bg-color-1);
-		border: 1px solid var(--bg-color-3);
-	}
-
-	#transport-controls {
-		display: flex;
-		border-bottom: 1px solid var(--bg-color-3);
-	}
-
-	#play-button {
-		cursor: pointer;
-
-		width: 1.5em;
-		height: 1.5em;
-		margin: .5em 0 0 .75em;
-		
-		box-sizing: border-box;
-		border-width: .75em 0 .75em 1.5em;
-		border-style: solid;
-
-		background: transparent;
-		border-color: transparent transparent transparent var(--bg-color-3);
-		filter: brightness(150%);
-	}
-
-	#play-button.paused { 
-		border-width: 0 0 0 1.5em;
-		border-style: double;
-	}
-
-	#play-button:hover {
-		filter: brightness(250%);
-	}
-
-	#transport-controls label[for="seek-slider"] {
-		font-family: monospace;
-		padding: .75em;
-		border-left: 1px solid var(--bg-color-2);
-		border-right: 1px solid var(--bg-color-2);
-	}
-
 	input[type="range"] {
 		flex: auto;
 		-webkit-appearance: none;
@@ -68,6 +21,88 @@ stylesheet.replaceSync(`
 		border-radius: 50%;
 		transform: translateY(-.5em);
 	}
+
+	#playlist-container {
+		background: var(--bg-color-1);
+		border: 1px solid var(--bg-color-3);
+	}
+
+	#player-controls {
+		display: flex;
+		border-bottom: 1px solid var(--bg-color-3);
+	}
+
+/* Play Button */
+
+	#play-button {
+		cursor: pointer;
+
+		width: 1.5em;
+		height: 1.5em;
+		margin: .55em -.1em 0 .75em;
+		
+		box-sizing: border-box;
+		border-width: .75em 0 .75em 1.5em;
+		border-style: solid;
+
+		background: transparent;
+		border-color: transparent transparent transparent var(--bg-color-3);
+		filter: brightness(150%);
+	}
+
+	#play-button:hover {
+		filter: brightness(250%);
+	}
+
+	#play-button:active {
+		filter: brightness(200%);
+	}
+
+	#play-button.paused { 
+		border-width: 0 0 0 1.5em;
+		border-style: double;
+	}
+
+/* Loop Button */
+
+	#loop-checkbox {
+		display:none;
+	}
+
+	label[for="loop-checkbox"] {
+		border-left: 1px solid var(--bg-color-2);
+	}
+
+	label[for="loop-checkbox"]:hover {
+		filter: brightness(110%);
+	}
+
+	label[for="loop-checkbox"]::before {
+		font-size: 1.5em;
+
+		filter: grayscale(75%);
+
+		content: "🔁";
+	}
+
+	#loop-checkbox:checked + label[for="loop-checkbox"]::before {
+		content: "🔂";
+	}
+
+	#loop-checkbox:active + label[for="loop-checkbox"] {
+		filter: brightness(75%);
+	}
+
+/* Transport Time Clock */	
+	
+	label[for="seek-slider"] {
+		font-family: monospace;
+		padding: .75em;
+		border-left: 1px solid var(--bg-color-2);
+		border-right: 1px solid var(--bg-color-2);
+	}
+
+/* Volume Controls */
 
 	#volume-controls {
 		border-left: 1px solid var(--bg-color-2);
@@ -91,46 +126,52 @@ stylesheet.replaceSync(`
 		height: 100%;
 		padding-top: .25em;
 
+		background: inherit;
+
 		filter: grayscale(100%);
 	}
 
 	#volume-slider {
+		display: none;
+
 		position: relative;
 		left: 0;
-		top: -3.5em;
+		top: calc(-4em - 2px);
 		transform-origin: 0% 50%;
-		transform: rotate(-90deg) translateY(75%);
+		transform: rotate(-90deg) translateY(40%);
+		height: inherit;
 	
 		background: var(--bg-color-1);
 		border: 1px solid var(--bg-color-2);
-
-		z-index: -1;
 	}
 
 	#volume-toggle:checked ~ #volume-slider {
-		z-index: 1;
+		display: unset;
 	}
 
-	.playlist {
+/* Playlist Controls */
+
+	#playlist {
+		margin-bottom: .25em;
 		border: 1px solid var(--bg-color-3);
 	}
 
-	.playlist input[type="radio"] {
+	#playlist input[type="radio"] {
 		display: none;
 	}
 
-	.playlist label {
+	#playlist label {
 		cursor: pointer;
 		display: block;
 		padding: .25em .5em;
 		border-bottom: 1px solid var(--bg-color-3);
 	}
 
-	.playlist label:hover {
+	#playlist label:hover {
 		background: var(--bg-color-3);
 	}
 
-	.playlist input[type="radio"]:checked+label {
+	#playlist input[type="radio"]:checked+label {
 		color: var(--accent-color);
 		border-bottom: 1px solid var(--accent-color);
 	}
@@ -138,27 +179,21 @@ stylesheet.replaceSync(`
 
 const template = document.createElement("template");
 template.innerHTML = `
-<div class="playlist-container">
-	<div id="transport-controls">
+<div id="playlist-container">
+	<div id="player-controls">
 		<button id="play-button"></button>
-		<label for="seek-slider">0:00 / 0:00</label><input type="range" id="seek-slider" value="0" step="0.1">
+		<input type="checkbox" id="loop-checkbox"><label for="loop-checkbox" title="Loop"></label>
+		<label for="seek-slider">0:00 / 0:00</label><input type="range" id="seek-slider" value="0" step="0.001">
 		<div id="volume-controls" title="Volume Controls">
-			<input type="checkbox" id="volume-toggle"><label for="volume-toggle">&#128266</label>
+			<input type="checkbox" id="volume-toggle"><label for="volume-toggle"></label>
 			<input type="range" id="volume-slider" value="1" min="0" max="1" step="0.01">
 		</div>
 	</div>
 	
 	<audio id="player-audio"></audio>
-	<div class="playlist"></div>
-	<slot name="sources"></slot>
+	<div id="playlist"></div>
 </div>
 `
-
-const volume_emojis = [
-	{min: 0.5, code: 128266},
-	{min: 0, code: 128265},
-	{min: -Infinity, code: 128264}
-];
 
 class AudioPlaylist extends HTMLElement {
 	constructor() {
@@ -173,15 +208,15 @@ class AudioPlaylist extends HTMLElement {
 			seek_slider: shadowRoot.querySelector("#seek-slider"),
 			volume_label: shadowRoot.querySelector("label[for=volume-toggle]"),
 			volume_slider: shadowRoot.querySelector("#volume-slider"),
-			playlist: shadowRoot.querySelector(".playlist"),
+			playlist: shadowRoot.querySelector("#playlist"),
 		}
 
 		const timeupdate_callback = () => {
 			if (this.active_track.duration) this.updateSeekSlider();
 		}
 		
-		// NOTE: Is it possible to have more than one node assigned to the same slot?
-		this.sources = shadowRoot.querySelector(`slot[name="sources"]`).assignedNodes()[0],
+		this.sources = {};
+		this.index = [];
 
 		this.active_track = shadowRoot.querySelector("#player-audio");
 		this.active_track.onloadeddata = () => {
@@ -206,15 +241,18 @@ class AudioPlaylist extends HTMLElement {
 		}
 		seek_slider.oninput = () => { this.updateTimeClock(seek_slider.value, this.active_track.duration);}
 
+		this.controls.volume_label.innerHTML = "&#128266;";
 		this.controls.volume_slider.oninput = (e) => { 
 			this.active_track.volume = e.target.value;
-		
-			for (let icon of volume_emojis) {
-				if (e.target.value > icon.min) { 
-					this.controls.volume_label.innerHTML = `&#${icon.code}`;
-					break;
-				}
-			}
+			
+			this.controls.volume_label.innerHTML = 
+				e.target.value > 0.5 ? 	"&#128266;"	:
+				e.target.value > 0   ? 	"&#128265;" :
+										"&#128264;" ;
+		}
+
+		shadowRoot.querySelector("#loop-checkbox").onchange = (e) => {
+			this.active_track.loop = e.target.checked;
 		}
 	}
 
@@ -222,13 +260,16 @@ class AudioPlaylist extends HTMLElement {
 		if (this.isConnected && this.sources) {
 			let untitled_count = 0;
 			let first_item = true;
-			for (let audio of Array.from(this.sources.children)) {
+			for (let audio of Array.from(this.children)) {
 				if (audio.tagName != "SOURCE") continue;
 				let title = audio?.getAttribute("title") || audio?.getAttribute("src");
 				if (!title) title = "Untitled" + ++untitled_count;
 
 				let id = title.replace(" ", "-") + "-radio";
 				
+				this.sources[title] = audio;
+				this.index.push(title);
+
 				this.controls.playlist.insertAdjacentHTML("beforeend", `
 					<input type="radio" id="${id}" name="playlist-radio-selection" ${first_item ? "checked" : ""}><label for="${id}">${title}</label>
 				`);
@@ -249,16 +290,15 @@ class AudioPlaylist extends HTMLElement {
 	setActiveTrack(title) {
 		if (this?.active_track?.title === title) return;
 
-		let source = this.sources.querySelector(`source[title="${title}"]`);
+		let source = this.sources[title];
 		if (source) {
 			if (this.active_track) {
 				this.active_track.pause();
 				this.active_track.currentTime = 0;
-				this.sources.append(this.active_track.querySelector("source") || "");
 			}
 
 			this.controls.seek_slider.value = 0;
-			this.active_track.append(source);
+			this.active_track.src = source.src;
 			this.active_track.load();
 		}
 	}
